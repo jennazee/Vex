75ca68e0d74e930356d555b5bfd7270a515c11d5
Jenna Zeigen <jenna.zeigen@gmail.com>
2012-12-18 15:12:09 -0500

    initial commit

---
+++ vex.js
@@ -0,0 +1,120 @@
+// each, map, reduce, extend (with deep copy), filter, sameAs,
+
+var vex = { }
+
+vex.isArray = function(toTest) {
+    if (toTest.length && typeof(toTest) !== 'string') {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+// performs the function for each element in an object
+vex.each = function(data, funct, context) {
+    //array?
+    if (data.length) {
+        for (var i = 0; i < data.length; i++) {
+            funct.call(context, data[i], i, data);
+        }
+    }
+    //object
+    else {
+        for (var key in data){
+            funct.call(context, data[key], key, data);
+        }
+    }
+};
+
+//returns a LIST of mapped values from an object
+vex.map = function(data, funct, context) {
+    var result = []
+    vex.each(data, function(val, index, list) {
+        result[result.length] = funct.call(context, val, index, context);
+    })
+    return result
+};
+
+// var reduce = function(data, accum, function, context) {
+
+// }
+
+vex.sameAs = function(data1, data2) {
+    //catches strings and integers
+    if (data1 === data2) {
+        return true
+    }
+
+    if typeof(data1) !== typeof(data2) {
+        return false;
+    }
+
+    // for arrays
+    if (data1.length && data2.length) {
+        if (data1.length !== data2.length) {
+            return false;
+        }
+        else {
+            for (var i = 0; i < data1.length; i++) {
+                if data1[i] !== data2[i] {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    if 
+
+}
+
+// _.map = _.collect = function(obj, iterator, context) {
+//     var results = [];
+
+//     if (obj == null) {
+//         return results;
+//     }
+
+//     if (nativeMap && obj.map === nativeMap) {
+//         return obj.map(iterator, context);
+//     }
+
+//     each(obj, function(value, index, list) {
+//         results[results.length] = iterator.call(context, value, index, list);
+//     });
+
+//     return results;
+// };
+
+
+
+
+// var each = _.each = _.forEach = function(obj, iterator, context) {
+//     if (obj == null) {
+//         return;
+//     }
+
+//     if (nativeForEach && obj.forEach === nativeForEach) {
+//         obj.forEach(iterator, context);
+//     }
+
+//     else if (obj.length === +obj.length) {
+//         for (var i = 0, l = obj.length; i < l; i++) {
+//             if (iterator.call(context, obj[i], i, obj) === breaker){
+//                 return;
+//             }
+//         }
+//     }
+
+//     else {
+//         for (var key in obj) {
+//             if (_.has(obj, key)) {
+//                 if (iterator.call(context, obj[key], key, obj) === breaker) {
+//                     return;
+//                 }
+//             }
+//          }
+//     }
+
+// };
